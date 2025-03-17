ba2ed7190297fc3ea4f28f80e406adb2a0b49f4c
3λΞĐ <3yed1361@gmail.com>
2025-03-17 21:57:48 +0330

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// ...existing code...
-function translateText(input, language) {
-    // Simulate translation logic
-    return `Translated "${input}" to ${language}`;
-}
-
-module.exports = { translateText };
-// ...existing code...
